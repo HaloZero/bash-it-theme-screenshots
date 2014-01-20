2d6bd6baade5042af12000c75f5a429f9aa95eb8
rohan <halo.zero00@gmail.com>
2014-01-20 13:43:42 -0800

    Adding TODO enhancement for future

--- bash-screenshot.rb
+++ bash-screenshot.rb
@@ -9,6 +9,9 @@ bash_file = File.expand_path('~/.bash_profile')
 
 themes.each do |theme|
   p "creating theme #{theme}"
+
+  # TODO: Future enhancement: Generate individual theme bash_profiles
+  #  Then run bash --init-file instead?
   bash_profile = File.read(bash_file)
   new_theme_string = "export BASH_IT_THEME='#{theme}'"
   new_profile_text = bash_profile.gsub(/export BASH_IT_THEME='.*'/, new_theme_string)
